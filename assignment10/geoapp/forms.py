7f2924fae81e76758a5de0b0c5ab573e27101911
ezgna <fjpfbii4@gmail.com>
2025-11-22 03:54:00 -0800

    Add basic Django Geo Weather Adventure app

---
+++ assignment10/geoapp/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+class ContinentForm(forms.Form):
+    CONTINENT_CHOICES = [
+        ('Africa', 'Africa'),
+        ('Americas', 'Americas'),
+        ('Asia', 'Asia'),
+        ('Europe', 'Europe'),
+        ('Oceania', 'Oceania'),
+    ]
+    continent = forms.ChoiceField(
+        choices=CONTINENT_CHOICES,
+        label="Continent"
+    )
